d55999502b206d16933327fe97f124e7f4c1c4da
camdelay16 <camdelay16@gmail.com>
2024-11-18 09:42:44 -0500

    created events model and updated index.js file with event path.

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const EventSchema = require("./event");
 
 // Add models with the imported Schemas below
+const Event = mongoose.model("Event", EventSchema);
 
-module.exports = {
-
-}
+module.exports = {};
