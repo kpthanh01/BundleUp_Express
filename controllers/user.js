c2f1751c3ac40b51a2fa2bdbe7018fb01f087748
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-11-18 12:10:16 -0600

    User controller with create, update, delete, and sign in

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,74 @@
 const express = require('express')
 const router = express.Router()
-//import the model
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-// Add all the CRUD features bellow
+
+const SALT_LENGTH = 12;
+
+router.post('/signup', async (req, res) => {
+    console.log(req.body)
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username })
+        if (userInDatabase) {
+            return res.json({error: 'Username already taken.'})
+        }
+        const user = await User.create({
+            username: req.body.username,
+            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+        })
+        const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET)
+        res.status(201).json({ user, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+
+router.post('/signin', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+            const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+            res.status(200).json({ token });
+        } else {
+            res.status(401).json({ error: 'Invalid username or password.' })
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+
+router.delete("/:userId", async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.userId)
+        if (!deletedUser) {
+            res.status(404)
+            throw new Error("User not found")
+          }
+        res.status(200).json(deletedUser)
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.put("/:userId", async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        req.body,
+        { new: true }
+      )
+      if (!updatedUser) {
+        res.status(404)
+        throw new Error("User not found")
+      }
+      res.status(200).json(updatedUser)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  })
 
 module.exports = router
