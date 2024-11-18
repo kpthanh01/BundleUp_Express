423e883f7b98b10313fb9838eee6d27387d277c4
kpthanh01 <kphamthanh01@gmail.com>
2024-11-18 09:23:58 -0500

    added comment model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+const commentSchema = require('./comment')
 
 // Add models with the imported Schemas below
+const Comment = mongoose.model('Comments', commentSchema)
 
 module.exports = {
-
+  Comment
 }
